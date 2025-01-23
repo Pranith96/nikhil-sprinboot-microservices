a5479dba6a34430ec19d9bc3ffbdbd9357ba22e6
Pranith96 <ramannagaripranith96@gmail.com>
2025-01-23 14:36:06 +0530

    added get apis

--- bank-application/bank-application/src/main/java/com/bank/service/CustomerService.java
+++ bank-application/bank-application/src/main/java/com/bank/service/CustomerService.java
@@ -1,10 +1,20 @@
 package com.bank.service;
 
+import java.util.List;
+
 import com.bank.dto.CustomerRequest;
 import com.bank.dto.CustomerResponse;
+import com.bank.entity.Account;
+import com.bank.entity.Customer;
 
 public interface CustomerService {
 
 	CustomerResponse createCustomer(CustomerRequest customer);
+
+	List<Account> getAccountsForCustomer(long customerId);
+
+	Customer getCustomerInfo(long customerId);
+
+	List<Customer> getAllCustomerInfo();
 
 }
